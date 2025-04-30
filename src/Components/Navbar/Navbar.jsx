e765ea9a469602c19d919619314f2033117b552e
Suprio-Das <priyamd507@gmail.com>
2025-04-30 23:27:51 +0600

    VibeReadme logo and title updated of navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import Logo from '../../assets/VibeReadmeLogo.png'
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100 shadow-sm">
-            <a className="btn btn-ghost text-xl">VibeReadme</a>
+        <div className="navbar bg-[#171612] shadow-sm">
+            <div className='w-[90%] mx-auto flex items-center gap-2'>
+                <img src={Logo} alt="Logo" className='w-11' />
+                <a className="text-xl text-white">VibeReadme</a>
+            </div>
         </div>
     );
 };
